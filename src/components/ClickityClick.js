9c56c245f38c0967e016fd38223ffd70344e4d17
Christopher Kim <chris.kim1990@gmail.com>
2021-05-21 16:50:56 -0400

    clicked and not clicked

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,33 @@
-// Code ClickityClick Component Here
+import React, {Component} from 'react';
+
+class ClickityClick extends Component {
+    constructor(){
+        super()
+        //initial state
+        this.state = {
+            hasBeenClicked: false
+        }
+    }
+    //when handleClick is called, setState will update the state
+    handleClick = () => {
+        if(this.state.hasBeenClicked === false){
+            this.setState({
+                hasBeenClicked: true
+            })
+        } else {
+            this.setState({
+                hasBeenClicked: false
+            })
+        }
+    }
+
+    render() {
+        return (
+            <div onClick={this.handleClick}>
+                <p>I have {this.state.hasBeenClicked ? null : 'not'} been clicked!</p>
+            </div>
+        )
+    }
+}
+
+export default ClickityClick
